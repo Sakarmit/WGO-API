8966e602a8a0918c1c559293636a86313bb1979a
CaptainIndia <ayirakas28@gmail.com>
2023-07-29 20:20:42 -0400

    Something about overposting
    based on the microsoft tutorial

--- Controllers/MarkersController.cs
+++ Controllers/MarkersController.cs
@@ -22,18 +22,20 @@ namespace WGO_API.Controllers
 
         // GET: api/Markers
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Marker>>> GetMarkers()
+        public async Task<ActionResult<IEnumerable<MarkerDTO>>> GetMarkers()
         {
-          if (_context.Markers == null)
-          {
-              return NotFound();
-          }
-            return await _context.Markers.ToListAsync();
+            if (_context.Markers == null)
+            {
+                return NotFound();
+            }
+            return await _context.Markers
+              .Select(x => MarkerToDTO(x))
+              .ToListAsync();
         }
 
         // GET: api/Markers/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Marker>> GetMarker(long id)
+        public async Task<ActionResult<MarkerDTO>> GetMarker(long id)
         {
             if (_context.Markers == null)
             {
@@ -46,35 +48,35 @@ namespace WGO_API.Controllers
                 return NotFound();
             }
 
-            return marker;
+            return MarkerToDTO(marker);
         }
 
         // PUT: api/Markers/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutMarker(long id, Marker marker)
+        public async Task<IActionResult> PutMarker(long id, MarkerDTO markerDTO)
         {
-            if (id != marker.Id)
+            if (id != markerDTO.Id)
             {
                 return BadRequest();
             }
 
-            _context.Entry(marker).State = EntityState.Modified;
+            var marker = await _context.Markers.FindAsync(id);
+            
+            if (marker == null)
+            {
+                return NotFound();
+            }
+
+            updateMarker(marker, markerDTO);
 
             try
             {
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException)
+            catch (DbUpdateConcurrencyException) when (!MarkerExists(id))
             {
-                if (!MarkerExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return NotFound();
             }
 
             return NoContent();
@@ -83,16 +85,26 @@ namespace WGO_API.Controllers
         // POST: api/Markers
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Marker>> PostMarker(Marker marker)
+        public async Task<ActionResult<MarkerDTO>> PostMarker(Marker markerDTO)
         {
-          if (_context.Markers == null)
-          {
-              return Problem("Entity set 'MarkerContext.Markers'  is null.");
-          }
+            var marker = new Marker
+            {
+                Id = markerDTO.Id,
+                User = markerDTO.User,
+                latitude = markerDTO.latitude,
+                longitude = markerDTO.longitude,
+                DateTime = markerDTO.DateTime,
+                Summary = markerDTO.Summary,
+                Title = markerDTO.Title
+            };
+            
             _context.Markers.Add(marker);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetMarker), new { id = marker.Id }, marker);
+            return CreatedAtAction(
+                nameof(GetMarker),
+                new { id = marker.Id },
+                MarkerToDTO(marker));
         }
 
         // DELETE: api/Markers/5
@@ -118,6 +130,30 @@ namespace WGO_API.Controllers
         private bool MarkerExists(long id)
         {
             return (_context.Markers?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        //Converts the Marker object to MarkerDTO hiding non-public info
+        private static MarkerDTO MarkerToDTO(Marker marker)
+        => new MarkerDTO
+        {
+            Id = marker.Id,
+            User = marker.User,
+            //Not adding reportCount
+            latitude = marker.latitude,
+            longitude = marker.longitude,
+            DateTime = marker.DateTime,
+            Summary = marker.Summary,
+            Title = marker.Title
+        };
+
+        //Update the marker entry with info from markerDTO
+        private static void updateMarker(Marker marker, MarkerDTO markerDTO)
+        {
+            marker.Title = markerDTO.Title;
+            marker.Summary = markerDTO.Summary;
+            marker.latitude = markerDTO.latitude;
+            marker.longitude = markerDTO.longitude;
+            marker.DateTime = markerDTO.DateTime;
         }
     }
 }
--- Models/MarkerDTO.cs
+++ Models/MarkerDTO.cs
@@ -2,10 +2,9 @@ using Microsoft.CodeAnalysis;
 
 namespace WGO_API.Models
 {
-    public class Marker
+    public class MarkerDTO
     {
         public required long Id { get; set; }
-        public byte reportCount { get; set; }
         public required string User { get; set; }
         public required decimal longitude { get; set; }
         public required decimal latitude { get; set; }
